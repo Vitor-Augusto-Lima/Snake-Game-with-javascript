04ddbcf1a049e085f229be0748babcc066ac54b6
Vitor Augusto <vitoraugustolimaa@gmail.com>
2021-11-02 12:43:02 -0300

    snake direction

--- Main.js
+++ Main.js
@@ -6,6 +6,7 @@ snake[0] = {
     x:8*box,
     y:8*box
 }
+let direction = "right";
 
 function criarBG() {
     context.fillStyle = "#31A719";
@@ -20,5 +21,42 @@ function criarSnake() {
     }
 }
 
+document.addEventListener('keydown', upadate)
+
+function upadate(event) {
+    if(event.keyCode == 37 && direction != "right") direction = "left"
+    if(event.keyCode == 38 && direction != "down") direction = "up"
+    if(event.keyCode == 39 && direction != "left") direction = "right"
+    if(event.keyCode == 40 && direction != "up") direction = "down"
+}
+
+
+function iniciarjogo() {
+if(snake[0].x > 15*box && direction == "right") snake[0].x = 0;
+if(snake[0].x < 0 && direction == "left") snake[0].x = 16*box;
+if(snake[0].y > 15*box && direction == "down") snake[0].y = 0;
+if(snake[0].y < 0 && direction == "up") snake[0].y = 16*box;
+
 criarBG();
 criarSnake();
+
+let snakex = snake[0].x;
+let snakey = snake[0].y;
+
+if(direction == "right") snakex += box;
+if(direction == "left") snakex -= box;
+if(direction == "up") snakey -= box;
+if(direction == "down") snakey += box;
+
+snake.pop();
+
+let newhead = {
+    x: snakex,
+    y: snakey
+}
+
+snake.unshift(newhead);
+
+}
+
+let jogo = setInterval(iniciarjogo, 100)
