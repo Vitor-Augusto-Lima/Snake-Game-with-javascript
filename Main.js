dc9f5d7bcd7d81fd7c5c4ea34446abb647ee490c
Vitor Augusto <vitoraugustolimaa@gmail.com>
2021-11-05 10:46:04 -0300

    snake game done

--- Main.js
+++ Main.js
@@ -46,6 +46,13 @@ if(snake[0].x < 0 && direction == "left") snake[0].x = 16*box;
 if(snake[0].y > 15*box && direction == "down") snake[0].y = 0;
 if(snake[0].y < 0 && direction == "up") snake[0].y = 16*box;
 
+for(c=1; c<snake.length; c++) {
+    if(snake[0].x == snake[c].x && snake[0].y == snake[c].y){
+        clearInterval(jogo);
+        alert("Game Over");
+    }
+}
+
 criarBG();
 criarSnake();
 drawfood()
